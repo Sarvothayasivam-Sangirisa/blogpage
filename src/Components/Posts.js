23c2e50713d1876ea6ec47b190d8b97eda81d4c8
Sarvothayasivam-Sangirisa <sangiri15sivam@gmail.com>
2024-07-25 16:46:46 +0530

    3rd commit

--- src/Components/Posts.js
+++ src/Components/Posts.js
@@ -8,12 +8,7 @@ import Post1 from './Post1';
 function Posts() {
   return (
     <div>
-      {/* <div className="box">
-      <div className="box1"><a href="#post1">JAVASCRIPT </a></div>
-      <div className="box1"> <a href="#post3">Algorithm</a></div>
-      <div className="box1"><a href="#post4">ComputerNetwork</a></div>
-      <div className="box1"><a href="#post2">Data Structure</a></div>
-    </div> */}
+
       <Post1/>
       <Post2/>
       <Post3/>
